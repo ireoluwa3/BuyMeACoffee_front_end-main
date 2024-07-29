02b9f5c4b93e744d71827d30a063f07c4d81e580
ireoluwa3 <62389303+ireoluwa3@users.noreply.github.com>
2024-07-29 16:37:11 +0100

    Update _app.jsx

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,40 +1,16 @@
+import { Web3ReactProvider } from '@web3-react/core';
+import { Web3Provider } from '@ethersproject/providers';
 import '../styles/globals.css';
-import { EthereumClient, w3mConnectors, w3mProvider } from '@web3modal/ethereum';
-import { Web3Modal } from '@web3modal/react';
-import { configureChains, createConfig, WagmiConfig } from 'wagmi';
-import { sepolia } from 'wagmi/chains';
 
-if (!process.env.WALLET_CONNECT_PROJECT_ID) {
-  throw new Error('You need to provide WALLET_CONNECT_PROJECT_ID env variable');
+function getLibrary(provider) {
+  return new Web3Provider(provider);
 }
-
-const chains = [sepolia];
-const projectId = process.env.WALLET_CONNECT_PROJECT_ID;
-
-const { publicClient } = configureChains(chains, [w3mProvider({ projectId })]);
-const wagmiConfig = createConfig({
-  autoConnect: true,
-  connectors: w3mConnectors({ projectId, version: 1, chains }),
-  publicClient,
-});
-const ethereumClient = new EthereumClient(wagmiConfig, chains);
 
 function MyApp({ Component, pageProps }) {
   return (
-    <>
-      <WagmiConfig config={wagmiConfig}>
-        <Component {...pageProps} />
-      </WagmiConfig>
-
-      <Web3Modal
-        projectId={projectId}
-        ethereumClient={ethereumClient}
-        themeVariables={{
-          '--w3m-background-color': '#C58940',
-          '--w3m-accent-color': '#C58940',
-        }}
-      />
-    </>
+    <Web3ReactProvider getLibrary={getLibrary}>
+      <Component {...pageProps} />
+    </Web3ReactProvider>
   );
 }
 

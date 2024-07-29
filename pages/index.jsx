d6c5faa61415e7f81054a4dd5b82881ffba1bda1
ireoluwa3 <62389303+ireoluwa3@users.noreply.github.com>
2024-07-29 16:31:10 +0100

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,50 +1,13 @@
-import Head from 'next/head'
-import React, { useEffect, useState } from "react";
-import styles from "../styles/index.module.css";
-import Main from '../components/Main'
-import Card from '../components/Card'
-import Footer from '../components/Footer'
-import { contractAddress, contractABI } from '../utils/contractInfo.js'
-import { useContractRead } from 'wagmi'
+import React from 'react';
+import ConnectButton from '../components/ConnectButton';
 
-
-export default function Home() {
-
-  const { data: memos, refetch: refetchMemos, isFetched } = useContractRead({
-    address: contractAddress,
-    abi: contractABI,
-    functionName: 'getMemos',
-    onSuccess(data) {
-      console.log('Success getMemos', data)
-    },
-    onError(error) {
-      console.log('Error getMemos', error)
-    },
-  })
-
+const Home = () => {
   return (
-
-    <div className={styles.main}>
-      <Head>
-        <title className={styles.title}>Buy Leonardo a Coffee!</title>
-        <meta name="description" content="Tipping site" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <Main refetchMemos={refetchMemos}/>
-
-      <div className={styles.grid}>
-        {isFetched && (memos.map((memo, idx) => {
-          const timestamp = new Date(memo.timestamp.toString() * 1000);
-          return (
-            <Card key={idx} id={idx} name={memo.name} message={memo.message} timestamp={timestamp} />
-          )
-        }))}
-      </div>
-
-      <Footer />
-
+    <div>
+      <h1>Buy Me a Coffee</h1>
+      <ConnectButton />
     </div>
-  )
+  );
+};
 
-}
+export default Home;
